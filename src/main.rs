ec570220f581570269912d0396a4ae7fcbbde96b
Rahmat Ashari <rahx@utexas.edu>
2023-01-25 18:31:44 +0700

    add search fns

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env; 
 use std::process;
 use std::fs;
-use std::error::Error;
+use colored::Colorize;
 
 fn main() {
     let arg_input: Vec<String> = env::args().collect();
@@ -10,12 +10,48 @@ fn main() {
         process::exit(1);
     });
     let text = read_file(&config.filename);
+    let result = run(&config, &text);
+    if result.len() == 0 {
+        println!("{}", "no line matches the given query".red())
+    } else {
+        println!("{:#?}", result)
+    }
+}
+
+fn run<'a>(config: &Config, content: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    if config.is_case_sensitive {
+        result = search_case_sensitive(&config.query, &content);
+    } else {
+        result = search_case_insensitive(&config.query, &content);
+    }
+    result
 }
 
 fn read_file(filename: &str) -> String {
     let file_content = fs::read_to_string(filename).expect("cannot read file");
-    println!("text : \n{}", file_content);
     file_content
+}
+
+fn search_case_sensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = Vec::new();
+    for line in content.lines(){
+        if line.contains(query) {
+            result.push(line)
+        }
+    }
+    result
+}
+
+fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = Vec::new();
+    let query = query.to_lowercase();
+    for line in content.lines(){
+        if line.to_lowercase().contains(&query) {
+            result.push(line)
+        }
+    }
+    result
 }
 
 #[derive(Debug)]
